59e9af4b11d4693f89a46b3ac20ad96a5cad4ad9
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-30 12:21:23 +0100

    Initial commit

--- pages.js
+++ pages.js
@@ -18,6 +18,30 @@ var pages = [
     nameOriginal: 'politikk.forside.png',
     urlNew: 'http://telemark.vangenplotz.net/tfk/Politikk',
     nameNew:'politikk.forside.ny.png'
+  },
+  {
+    urlOriginal: 'http://www.telemark.no/Fylkeskommunen',
+    nameOriginal: 'fylkeskommunen.forside.png',
+    urlNew: 'http://telemark.vangenplotz.net/tfk/Fylkeskommunen',
+    nameNew:'fylkeskommunen.forside.ny.png'
+  },
+  {
+    urlOriginal: 'http://www.telemark.no/Planer',
+    nameOriginal: 'planer.forside.png',
+    urlNew: 'http://telemark.vangenplotz.net/tfk/Planer',
+    nameNew:'planer.forside.ny.png'
+  },
+  {
+    urlOriginal: 'http://www.telemark.no/Vaare-tjenester',
+    nameOriginal: 'vaare.tjenester.forside.png',
+    urlNew: 'http://telemark.vangenplotz.net/tfk/Vaare-tjenester',
+    nameNew:'vaare.tjenester.forside.ny.png'
+  },
+  {
+    urlOriginal: 'http://www.telemark.no/Aktuelt',
+    nameOriginal: 'aktuelt.forside.png',
+    urlNew: 'http://telemark.vangenplotz.net/tfk/Aktuelt',
+    nameNew:'aktuelt.forside.ny.png'
   }
 ];
 
